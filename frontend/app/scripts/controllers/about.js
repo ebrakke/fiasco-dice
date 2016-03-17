9284fece4b6cd5399c0ce897cb3d51ed0ec9a8c7
Erik Brakke <eriksbrakke@gmail.com>
2016-03-17 16:16:32 -0400

    Initial commit

---
+++ frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fiascoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fiascoApp
+ */
+angular.module('fiascoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
